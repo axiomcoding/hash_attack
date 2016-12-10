3717605ccf32dfe8ad8d592be3fdc206b7a16d2e
Michael Tejada <michael@axiomcoding.com>
2016-12-10 12:04:20 -0700

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hash Attack
+Refer to, but do not alter, the global `$capitals` hash predefined in `hash_attack.rb`. Access and manipulate the values specified in the RSpec file to pass each test. 
---
+++ hash_attack.rb
@@ -0,0 +1,57 @@
+# DO NOT ALTER THE FOLLOWING HASH
+$capitals = {
+  :arizona => "Scottsdale",
+  :california => "Sacramento",
+  :texas => "Austin",
+  :florida => "Tallassee",
+  :colorado => "Denver",
+  :michigan => "Lansing",
+  :hawaii => "Honolulu",
+  :utah => "Salt Lake City"
+}
+
+# define a method, key_count, that returns the number of keys
+
+
+
+
+
+
+# define a method, value_count, that returns the number of values 
+
+
+
+
+
+# define a method, add_oregon, that adds oregon and its capital, Salem to the hash and returns the hash
+
+
+
+
+
+
+# define a method, delete_utah, that deletes utah entirely from the hash
+
+
+
+
+
+# define a method, upcase, that upcases the "hawaii" capital
+
+
+
+
+
+
+# define a method, update_arizona, that updates that arizona capital to Phoenix
+
+
+
+
+
+
+# define a method, print_states_capitals, that converts the capitals hash to an array using the .to_a method
+# iterate through the arrays to print out: "The capital of ____ is _____." for each state and capital 
+# note: you must complete the previous methods so your hash has the most up to date values
+# hint: http://docs.ruby-lang.org/en/2.0.0/Hash.html#method-i-to_a
+
